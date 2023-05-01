996c1bb67653fd814acf1f8b1994a1d835a12049
bagusuntoro <untorobagus65@gmail.com>
2023-05-01 12:02:48 +0700

    finish timing vote

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -18,38 +18,62 @@ class VoteController extends Controller
 
     public function index()
     {
-        $votes = $this->voteService->getResults();
 
-        return response()->json([
-            'data' => $votes
-        ]);
+        $current_time = Carbon::now();
+        $timing_votes = TimingVote::orderBy('id', 'desc')->first();
+            
+        $startTime = Carbon::createFromFormat('Y-m-d H:i:s', $timing_votes->start_time, 'Asia/Jakarta');
+        $endTime = Carbon::createFromFormat('Y-m-d H:i:s', $timing_votes->end_time, 'Asia/Jakarta');
+        $current_time = Carbon::now('Asia/Jakarta');
+
+        if ($current_time->isBefore($startTime)) {
+            return response()->json([
+                'message' => 'Belum saatnya pemilihan dimulai.',
+            ]);
+        } elseif ($current_time->isAfter($endTime)) {
+            $votes = $this->voteService->getResults();
+
+            return response()->json([
+                'data' => $votes
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Maaf pemilihan masih berlangsung',
+            ]);
+        }
+
+
     }
 
     public function store(Request $request)
     {
-        $timingVote = TimingVote::first();
+        $current_time = Carbon::now();
+        $timing_votes = TimingVote::orderBy('id', 'desc')->first();
+            
+        $startTime = Carbon::createFromFormat('Y-m-d H:i:s', $timing_votes->start_time, 'Asia/Jakarta');
+        $endTime = Carbon::createFromFormat('Y-m-d H:i:s', $timing_votes->end_time, 'Asia/Jakarta');
+        $current_time = Carbon::now('Asia/Jakarta');
 
-        $data = json_decode($timingVote);
-        $startTime = Carbon::parse($data->start_time);
-        $endTime = Carbon::parse($data->end_time);
-        $currentTime = Carbon::now();
-
-        
-        if (!$currentTime->between($startTime, $endTime)) {
+        if ($current_time->isBefore($startTime)) {
             return response()->json([
-                'message' => 'Maaf, waktu pemilihan sudah berakhir.'
+                'message' => 'Belum saatnya pemilihan dimulai.',
+                'startTime' => $startTime->toIso8601String(),
+                'endTime' => $endTime->toIso8601String(),
+                'current_time' => $current_time->toIso8601String(),
+            ]);
+        } elseif ($current_time->isAfter($endTime)) {
+            return response()->json(['message' => 'Maaf, waktu pemilihan sudah berakhir.']);
+        } else {
+            $validatedData = $request->validate([
+                'voter_id' => 'required',
+                'candidate_id' => 'required',
+            ]);
+
+            $vote = $this->voteService->voteCandidate($request);
+
+            return response()->json([
+                'data' => $vote
             ]);
         }
-
-        $validatedData = $request->validate([
-            'voter_id' => 'required',
-            'candidate_id' => 'required',
-        ]);
-
-        $vote = $this->voteService->voteCandidate($request);
-
-        return response()->json([
-            'data' => $vote
-        ]);
     }
 }
