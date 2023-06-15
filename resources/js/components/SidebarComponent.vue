115e847b6e73aaad3f730b4ac9048e46d3e4562f
bagusuntoro <untorobagus65@gmail.com>
2023-06-15 20:49:20 +0700

    test

--- resources/js/components/SidebarComponent.vue
+++ resources/js/components/SidebarComponent.vue
@@ -1,84 +1,106 @@
 <template>
-    <div class="sidebar-wrapper" data-simplebar="true">
-     <div class="sidebar-header">
-         <div>
-             <img src="assets/images/logo-img.jpeg" class="logo-icon" alt="logo icon">
-         </div>
-         <div>
-             <h4 class="logo-text">EtoV</h4>
-         </div>
-         <div class="toggle-icon ms-auto"><i class='bx bx-first-page'></i>
-         </div>
-     </div>
-     <!--navigation-->
-     <ul class="metismenu" id="menu">
-         <li>
-            <router-link to="/dashboard">
-                 <div class="parent-icon">
-                     <i class="bi bi-speedometer2"></i>
-                 </div>
-                 <div class="menu-title">Dashboard</div>
-            </router-link>
-             <!-- <router-link to="/dashboard">
-             </router-link> -->
-         </li>
-         <li>
-             <router-link to="/createcandidate">
-                 <div class="parent-icon">
-                 <i class="bi bi-person-add"></i>
-             </div>
-                 <div class="menu-title">Create Candidate</div>
-             </router-link>
-         </li>
-         <li>
-             <router-link to="/candidate">
-                 <div class="parent-icon">
-                 <i class="bi bi-people"></i>
-             </div>
-                 <div class="menu-title">Data Candidates</div>
-             </router-link>
-         </li>
-         <li>
-             <router-link to="/time">
-                 <div class="parent-icon">
-                 <i class="bi bi-clock"></i>
-             </div>
-                 <div class="menu-title">Time Vote</div>
-             </router-link>
-         </li>
-         <li>
-             <router-link to="/result">
-                 <div class="parent-icon">
-                 <i class="bi bi-bar-chart"></i>
-             </div>
-                 <div class="menu-title">Result</div>
-             </router-link>
-         </li>
-
-         
-         <li>
-             <router-link to="/voter">
-                 <div class="parent-icon">
-                 <i class="bi bi-person-check"></i>
-             </div>
-                 <div class="menu-title">Data Voter</div>
-             </router-link>
-         </li>
-     </ul>
-     <!--end navigation-->
- </div>
+  <div class="sidebar-wrapper" :class="{ 'sidebar-closed': isSidebarClosed }" data-simplebar="true">
+    <div class="sidebar-header">
+      <div>
+        <img src="assets/images/logo-img.jpeg" class="logo-icon" alt="logo icon">
+      </div>
+      <div>
+        <h4 class="logo-text">EtoV</h4>
+      </div>
+      <div class="toggle-icon ms-auto" @click="toggleSidebar"><i :class="sidebarIcon"></i></div>
+    </div>
+    <!--navigation-->
+    <ul class="metismenu" id="menu">
+      <li>
+        <router-link to="/dashboard">
+          <div class="parent-icon">
+            <i class="bi bi-speedometer2"></i>
+          </div>
+          <div class="menu-title">Dashboard</div>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/createcandidate">
+          <div class="parent-icon">
+            <i class="bi bi-person-add"></i>
+          </div>
+          <div class="menu-title">Create Candidate</div>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/candidate">
+          <div class="parent-icon">
+            <i class="bi bi-people"></i>
+          </div>
+          <div class="menu-title">Data Candidates</div>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/time">
+          <div class="parent-icon">
+            <i class="bi bi-clock"></i>
+          </div>
+          <div class="menu-title">Time Vote</div>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/result">
+          <div class="parent-icon">
+            <i class="bi bi-bar-chart"></i>
+          </div>
+          <div class="menu-title">Result</div>
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/voter">
+          <div class="parent-icon">
+            <i class="bi bi-person-check"></i>
+          </div>
+          <div class="menu-title">Data Voter</div>
+        </router-link>
+      </li>
+    </ul>
+    <!--end navigation-->
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isSidebarClosed: false,
+    };
+  },
+  computed: {
+    sidebarIcon() {
+      return this.isSidebarClosed ? 'bx bx-last-page' : 'bx bx-first-page';
+    },
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarClosed = !this.isSidebarClosed;
+    },
+  },
+};
+</script>
 
 <style>
 .menu-title {
-color: #000000;
+  color: #000000;
 }
 .logo-text {
-font-family: "Arial", sans-serif;
-font-weight: bold;
+  font-family: "Arial", sans-serif;
+  font-weight: bold;
 }
 .logo-icon {
-margin-right: 10px; 
+  margin-right: 10px;
 }
 
+.sidebar-closed {
+  width: 80px;
+}
+
+.sidebar-closed .toggle-icon {
+  transform: rotate(180deg);
+}
 </style>
--- resources/js/components/pages/LoginComponent.vue
+++ resources/js/components/pages/LoginComponent.vue
@@ -1,87 +1,67 @@
 <template>
-	<div class="wrapper">
-	  <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
-		<div class="container-fluid">
-		  <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-2">
-			<div class="col mx-auto">
-			  <div class="mb-4 text-center"></div>
-			  <div class="card">
-				<div class="card-body">
-				  <div class="p-4 rounded">
-					<div class="text-center">
-					  <h3 class="">Sign in</h3>
-					</div>
-					<div class="login-separater text-center mb-4">
-					  <span>SIGN IN WITH EMAIL</span>
-					  <hr />
-					</div>
-					<div class="form-body">
-					  <form class="row g-3" @submit.prevent="handleSubmit">
-						<div class="col-12">
-						  <label for="inputEmailAddress" class="form-label"
-							>Email Address</label
-						  >
-						  <input
-							type="email"
-							class="form-control"
-							id="inputEmailAddress"
-							placeholder="Email Address"
-							v-model="email"
-						  />
-						</div>
-						<div class="col-12">
-						  <label for="inputChoosePassword" class="form-label"
-							>Enter Password</label
-						  >
-						  <div class="input-group" id="show_hide_password">
-							<input
-							  type="password"
-							  class="form-control border-end-1"
-							  id="inputChoosePassword"
-							  v-model="password"
-							  placeholder="Enter Password"
-							/>
-						  </div>
-						</div>
-						<div class="col-md-6">
-						  <div class="form-check form-switch">
-							<input
-							  class="form-check-input"
-							  type="checkbox"
-							  id="flexSwitchCheckChecked"
-							  checked
-							/>
-							<label
-							  class="form-check-label"
-							  for="flexSwitchCheckChecked"
-							  >Remember Me</label
-							>
-						  </div>
-						</div>
-						<div class="col-12">
-						  <div class="d-grid">
-							<button
-							  type="submit"
-							  class="btn btn-primary"
-							  :disabled="!email || !password"
-							>
-							  <i class="bx bxs-lock-open"></i>Sign in
-							</button>
-						  </div>
-						</div>
-					  </form>
-					</div>
-				  </div>
-				</div>
-			  </div>
-			</div>
-		  </div>
-		  <!--end row-->
-		</div>
-	  </div>
-	</div>
-  </template>
-
+  <div class="wrapper">
+    <div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+      <div class="container-fluid">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-2">
+          <div class="col mx-auto">
+            <div class="mb-4 text-center"></div>
+            <div class="card">
+              <div class="card-body">
+                <div class="p-4 rounded">
+                  <div class="text-center">
+                    <h3 class="">Sign in</h3>
+                  </div>
+                  <div class="login-separater text-center mb-4">
+                    <span>SIGN IN WITH EMAIL</span>
+                    <hr />
+                  </div>
+                  <div class="form-body">
+                    <form class="row g-3" @submit.prevent="handleSubmit">
+                      <div class="col-12">
+                        <label for="inputEmailAddress" class="form-label">Email Address</label>
+                        <input
+                          type="email"
+                          class="form-control"
+                          id="inputEmailAddress"
+                          placeholder="Email Address"
+                          v-model="email"
+                        />
+                      </div>
+                      <div class="col-12">
+                        <label for="inputChoosePassword" class="form-label">Enter Password</label>
+                        <div class="input-group" id="show_hide_password">
+                          <input
+                            type="password"
+                            class="form-control border-end-1"
+                            id="inputChoosePassword"
+                            v-model="password"
+                            placeholder="Enter Password"
+                          />
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="d-grid">
+                          <button
+                            type="submit"
+                            class="btn btn-primary"
+                            :disabled="!email || !password"
+                          >
+                            <i class="bx bxs-lock-open"></i>Sign in
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--end row-->
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 import axios from 'axios';
@@ -95,36 +75,38 @@ export default {
     };
   },
   methods: {
-    async handleSubmit() {
-      try {
-        const response = await axios.post('https://voting.surabayawebtech.com/api/auth/login', {
-          email: this.email,
-          password: this.password
-        });
+    handleSubmit() {
+  axios.post('https://voting.surabayawebtech.com/api/auth/login', {
+    email: this.email,
+    password: this.password
+  })
+  .then(response => {
+    if (response.data.level === 'admin') {
+      localStorage.setItem('token', response.data.access_token);
+      localStorage.setItem('expires_in', response.data.expires_in);
+      this.$router.push("/dashboard");
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Login Gagal",
+        text: 'Maaf, Anda bukan admin!'
+      }).then(() => {
+        this.$router.push('/');
+      });
+    }
+  })
+  .catch(error => {
+    Swal.fire({
+      icon: "error",
+      title: "Login Gagal",
+      text: 'Email atau password yang Anda masukkan salah!'
+    }).then(() => {
+      this.$router.push('/');
+    });
+    console.log(error);
+  });
+},
 
-        if (response.data.level === 'admin') {
-          localStorage.setItem('token', response.data.access_token);
-          localStorage.setItem('expires_in', response.data.expires_in);
-          this.$router.push("/dashboard");
-        } else {
-          Swal.fire({
-            icon: "error",
-            title: "Login Gagal",
-			text: 'email atau password yang anda masukkan salah!'
-          }).then(() => {
-            this.$router.push('/');
-          });
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
   },
 };
-
 </script>
-
-
-
-
-
